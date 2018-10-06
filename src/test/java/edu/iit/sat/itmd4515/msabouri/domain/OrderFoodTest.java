13bdb5fe18a6d50c0dfe427469e925df91f2ccd7
msabouri@hawk.iit.edu <milad.sabouri@gmail.com>
2018-10-05 20:15:09 -0500

    add text cases for validation bean (OrderFood class) currency validation min validation

--- src/test/java/edu/iit/sat/itmd4515/msabouri/domain/OrderFoodTest.java
+++ src/test/java/edu/iit/sat/itmd4515/msabouri/domain/OrderFoodTest.java
@@ -8,6 +8,8 @@ package edu.iit.sat.itmd4515.msabouri.domain;
 import static edu.iit.sat.itmd4515.msabouri.domain.AbstractJPATest.emf;
 import java.math.BigDecimal;
 import java.util.GregorianCalendar;
+import java.util.Set;
+import javax.validation.ConstraintViolation;
 import org.junit.After;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -68,5 +70,55 @@ public class OrderFoodTest extends AbstractJPATest{
         // test the owning side of the relationship
         assertNotNull(findOrder.getOffer().getTitle());
         assertEquals("Offer Title", findOrder.getOffer().getTitle());
+    }
+    
+    @Test
+    public void priceIsNegativeTest(){
+        OrderFood order = new OrderFood(
+                new GregorianCalendar(2018, 9, 23).getTime(), 1, new BigDecimal("-1"));
+        System.out.println(order.toString());
+        
+        Set<ConstraintViolation<OrderFood>> constraintViolations = validator.validate(order);
+        assertEquals(1, constraintViolations.size());
+        
+        assertEquals("must be greater than or equal to 0", constraintViolations.iterator().next().getMessage());
+        
+        for(ConstraintViolation<OrderFood> bad : constraintViolations){
+            System.out.println(bad.toString() + " " + bad.getPropertyPath() + " " + bad.getMessage());
+        }
+    }
+    
+    @Test
+    public void priceHas2DigitsFractionTest(){
+        OrderFood order = new OrderFood(
+                new GregorianCalendar(2018, 9, 23).getTime(), 1, new BigDecimal("56.988"));
+        System.out.println(order.toString());
+        
+        Set<ConstraintViolation<OrderFood>> constraintViolations = validator.validate(order);
+        assertEquals(1, constraintViolations.size());
+        
+        assertEquals("numeric value out of bounds (<4 digits>.<2 digits> expected)", constraintViolations.iterator().next().getMessage());
+        
+        for(ConstraintViolation<OrderFood> bad : constraintViolations){
+            System.out.println(bad.toString() + " " + bad.getPropertyPath() + " " + bad.getMessage());
+        }
+    }
+    
+    @Test
+    public void priceHas4DigitsIntegerTest(){
+        OrderFood order = new OrderFood(
+                new GregorianCalendar(2018, 9, 23).getTime(), 1, new BigDecimal("93489.02"));
+        
+        System.out.println(order.toString());
+        
+        Set<ConstraintViolation<OrderFood>> constraintViolations = validator.validate(order);
+        
+        assertEquals(1, constraintViolations.size());
+        
+        assertEquals("numeric value out of bounds (<4 digits>.<2 digits> expected)", constraintViolations.iterator().next().getMessage());
+        
+        for(ConstraintViolation<OrderFood> bad : constraintViolations){
+            System.out.println(bad.toString() + " " + bad.getPropertyPath() + " " + bad.getMessage());
+        }
     }
 }
